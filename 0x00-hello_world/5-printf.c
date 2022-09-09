c7fb0862ce2714e1321d80de47d6f875f916e528
Quwam Ogunlaja <quwam32@gmail.com>
2022-09-09 05:18:48 +0100

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: int
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
