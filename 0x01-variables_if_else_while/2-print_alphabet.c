3e82d6f341bd25b33424e3b8d3068c5e46703d22
Quwam Ogunlaja <quwam32@gmail.com>
2022-09-09 13:10:30 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: int
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
