1e91fceb841130c2c8e9c27169d145cb79b1234a
Quwam Ogunlaja <quwam32@gmail.com>
2022-09-09 05:32:27 +0100

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Returns: int
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int ll;
+	float f;
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(ll));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
