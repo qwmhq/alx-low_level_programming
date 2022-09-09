80ded78db509d012ac26820e75218aec1dc7f644
Quwam Ogunlaja <quwam32@gmail.com>
2022-09-09 15:57:27 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: int
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
