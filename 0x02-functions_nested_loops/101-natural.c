9d23ddc981c54e138f0db0a74badfa27ba0f2612
Quwam Ogunlaja <quwam32@gmail.com>
2022-09-14 00:34:25 +0100

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: always 0 
+ */
+int main(void)
+{
+	int sum, i;
+	
+	sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((!(i % 3)) || (!(i % 5)))
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
