f8ad943ee4bde2f652a4316892f1a71108a86904
Quwam Ogunlaja <quwam32@gmail.com>
2022-09-14 08:19:25 +0000

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: always 0
+ */
+int main(void)
+{
+	long int t1, t2;
+	long int n;
+	int i;
+
+	t1 = 1;
+	t2 = 2;
+
+	printf("%ld, %ld, ", t1, t2);
+
+	for (i = 3; i <= 50; i++)
+	{
+		n = t1 + t2;
+		t1 = t2;
+		t2 = n;
+		
+		printf("%ld", n);
+
+		if (!(i == 50))
+			printf(", ");
+		else
+			printf("\n");
+	}
+
+	return(0);
+}
