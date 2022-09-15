4e12a8e4d148256ac2d75f815daa7546d401fbff
Quwam Ogunlaja <quwam32@gmail.com>
2022-09-15 13:11:43 +0100

    corrected 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,9 @@ void print_most_numbers(void)
 
 	for (n = '0'; n <= '9'; n++)
 	{
-		if (n != 2 && n != 4)
+		if ((n != '2') && (n != '4'))
 			_putchar(n);
 	}
 
-	_putchar(n);
+	_putchar('\n');
 }
