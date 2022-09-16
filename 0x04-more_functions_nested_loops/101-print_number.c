36502a5a1d9a09a91c53cc4f63e342d946dd14f2
Quwam Ogunlaja <quwam32@gmail.com>
2022-09-16 06:30:02 +0100

    101-print_number.c, main.h

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_number - print an integer using only _putchar
+ * @n: the number to print
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if ((n / 10) > 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
