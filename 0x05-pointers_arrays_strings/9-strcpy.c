820ae63c68be51d4afddb7b5c336467a0238b430
Quwam Ogunlaja <quwam32@gmail.com>
2022-09-19 13:39:59 +0100

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcpy - copy one string to another
+ * @dest: pointer to the string to be copied to
+ * @src: pointer to the source string
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
