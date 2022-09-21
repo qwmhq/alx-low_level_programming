8a710968f56a869667fb8385e60e179621362ae8
Quwam Ogunlaja <quwam32@gmail.com>
2022-09-21 18:14:29 +0000

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: the first string
+ * @src: the second
+ * Return: the concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return strcat(dest, src);
+}
