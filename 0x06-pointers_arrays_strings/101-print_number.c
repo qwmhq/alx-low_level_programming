511aa3fb6f2d9cff6c2709eea72f2a7f4203608e
Quwam Ogunlaja <quwam32@gmail.com>
2022-09-22 09:30:57 +0100

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_number - print an integer using only _putchar
+ * @n: the number to print
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	num	= n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+	{
+		print_number(num / 10);
+	}
+
+	_putchar((num % 10) + '0');
+}
