fc55097c81aa2b95bed51a0c8f08800dc2835773
Quwam Ogunlaja <quwam32@gmail.com>
2022-10-01 05:09:21 +0100

    corrected 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,17 +6,13 @@
  * main - main function
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: always 0
+ * Return: 0 on success, 1 on failure
  */
 int main(int argc, char **argv)
 {
 	int i;
-	int amount;
-	int number_of_coins;
+	int amount, number_of_coins;
 	int coins[] = {25, 10, 5, 2, 1};
-
-	amount = atoi(argv[1]);
-	number_of_coins = 0;
 
 	if (argc != 2)
 	{
@@ -24,16 +20,16 @@ int main(int argc, char **argv)
 		return (1);
 	}
 
-	if (amount < 0)
-	{
-		printf("%d\n", 0);
-		return (0);
-	}
+	amount = atoi(argv[1]);
+	number_of_coins = 0;
 
-	for (i = 0; i < 5; i++)
+	if (amount > 0)
 	{
-		number_of_coins += (amount / coins[i]);
-		amount %= coins[i];
+		for (i = 0; i < 5; i++)
+		{
+			number_of_coins += (amount / coins[i]);
+			amount %= coins[i];
+		}
 	}
 
 	printf("%d\n", number_of_coins);
