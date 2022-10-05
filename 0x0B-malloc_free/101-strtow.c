3805ec2871ddb7c54086a4a0ad8e8caa789edc47
Quwam Ogunlaja <quwam32@gmail.com>
2022-10-05 10:23:05 +0100

    corrected 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,14 +14,13 @@ int count_words(char *str)
 
 	count = 0;
 
-	for (i = 0; i < strlen(str) - 1; i++)
+	for (i = 0; i < strlen(str); i++)
 	{
 		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 		{
 			count++;
 		}
 	}
-
 	return (count);
 }
 
