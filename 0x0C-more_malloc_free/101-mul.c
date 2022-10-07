077920c5e5e5be2416b84acd2e332e4faff98fba
Quwam Ogunlaja <quwam32@gmail.com>
2022-10-07 14:40:59 +0100

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * is_number - check if a string represents a number
+ * @str: the string
+ * Return: 1 if str is a number, otherwise 0
+ */
+int is_number(char *str)
+{
+	return (strspn(str, "1234567890") == strlen(str));
+}
+
+/**
+ * main - program entry
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	long long int n1, n2, result;
+
+	if (argc != 3 || !is_number(*(argv + 1)) || !is_number(*(argv + 2)))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	n1 = atoll(*(argv + 1));
+	n2 = atoll(*(argv + 2));
+	result = n1 * n2;
+
+	printf("%lld\n", result);
+
+	return (0);
+}
