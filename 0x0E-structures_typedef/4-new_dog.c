d27ffa2278d1c1ad9c310b2fe113bfb2afa5ace6
Quwam Ogunlaja <quwam32@gmail.com>
2022-10-10 17:12:44 +0100

    added new_dog function

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - create a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: pointer to the dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	char *namecpy, *ownercpy;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	namecpy = malloc((strlen(name) + 1) * sizeof(char));
+	ownercpy = malloc((strlen(owner) + 1) * sizeof(owner));
+	if (namecpy == NULL || ownercpy == NULL)
+		return (NULL);
+
+	d->name = strcpy(namecpy, name);
+	d->age = age;
+	d->owner = strcpy(ownercpy, owner);
+
+	return (d);
+}
