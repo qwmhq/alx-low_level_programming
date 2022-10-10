69b7c242689d613ef6489302a63b15d66ec31624
Quwam Ogunlaja <quwam32@gmail.com>
2022-10-10 16:11:59 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
