2cef96d44e0e161206c5122b44aa374f688d1c59
Quwam Ogunlaja <quwam32@gmail.com>
2022-10-12 20:43:46 +0000

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int i, no_of_bytes;
+	unsigned char *p;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	no_of_bytes = atoi(*(argv + 1));
+	p = (unsigned char *) main;
+
+	if (no_of_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < no_of_bytes; i++)
+	{
+		printf("%x ", *(p + i));
+	}
+
+	printf("\n");
+
+	return (0);
+}
