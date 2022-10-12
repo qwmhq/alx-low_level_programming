f13bac159ba3cddac036ef9b411352e5df820cea
Quwam Ogunlaja <quwam32@gmail.com>
2022-10-12 11:14:05 +0100

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name using a custom function
+ * @name: name to print
+ * @f: function to use in printing name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
