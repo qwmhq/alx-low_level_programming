df47b65881a8df682977a4f7f43182099eda1a4e
Quwam Ogunlaja <quwam32@gmail.com>
2022-10-13 15:43:25 +0100

    3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_all - print anything
+ * @format: format string
+ */
+void print_all(const char * const format, ...)
+{
+	size_t i;
+	va_list ap;
+	char *arg;
+
+	i = 0;
+	va_start(ap, format);
+	while (i < strlen(format))
+	{
+		switch (*(format + i))
+		{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				arg = va_arg(ap, char *);
+				if (arg == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", arg);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (i != (strlen(format) - 1))
+			printf(", ");
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
+}
