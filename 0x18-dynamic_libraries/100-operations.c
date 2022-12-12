71f4bbf7433463784623ff141436380d2b9980ce
Quwam Ogunlaja <quwam32@gmail.com>
2022-12-12 13:37:20 +0000

    100-operations

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,65 @@
+/**
+ * add - add two integers
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - find the difference between two integers
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * mul - multiply two integers
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two integers
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - find the modulo of two integers
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
