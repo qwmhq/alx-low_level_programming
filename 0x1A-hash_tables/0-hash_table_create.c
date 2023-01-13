2e35fa77b452a21af8541657b980cb7c7ec5892c
Quwam Ogunlaja <quwam32@gmail.com>
2023-01-13 08:46:01 +0100

    corrections

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,6 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (ht == NULL)
 		return (NULL);
+	ht->size = size;
 	ht->array = malloc(size * sizeof(hash_node_t *));
 	if (ht->array == NULL)
 	{
