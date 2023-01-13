cac27a4d3d838e6e97a0041db7ef98d30b40913c
Quwam Ogunlaja <quwam32@gmail.com>
2023-01-13 08:52:55 +0100

    key_index function

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - get the index of a key
+ *
+ * @key: the key
+ * @size: the size of the array of the hash table
+ *
+ * Return: the index of the key in the array
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
