cf398f17ac8e21f06956de9702c17bea79f19059
Quwam Ogunlaja <quwam32@gmail.com>
2023-01-13 08:34:45 +0100

    task 0 - hash_table_create

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table with given size
+ *
+ * @size: the size of the hash table
+ *
+ * Return: newly created hash table, or null if unsuccessful
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+
+	if (ht == NULL)
+		return (NULL);
+	ht->array = malloc(size * sizeof(hash_node_t *));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
+}
